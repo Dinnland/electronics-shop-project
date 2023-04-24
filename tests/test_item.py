e3852806a2caed8e9031a989d515b7f17e4a1f4f
dinar mingazov <dinarek2013@gmail.com>
2023-04-24 12:56:01 +0300

    homework 1, electronics shop item,test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 3)
+# Item.pay_rate = 0.5
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 60000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.5
+    item1.apply_discount()
+    item2.apply_discount()
+    assert item1.price == 5000
+    assert item2.price == 10000
