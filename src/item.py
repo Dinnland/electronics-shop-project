a37cfe50b5e5ddd6ae8fee0baf0cf5b26a65104a
Dinar Mingazov <dinarek2013@gmail.com>
2023-04-26 05:00:04 +0300

    домашка 2. режимы доступа

--- src/item.py
+++ src/item.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+
+    csv_file = Path.joinpath(Path(__file__).parent, 'items.csv')
+
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,10 +20,29 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name      # private name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property       # name getter
+
+
+    def name(self):
+        return self.__name
+
+    @name.setter     # name setter
+
+
+    def name(self, name):
+        try:
+            if len(name) > 10:
+                raise ValueError(
+                    'Длина наименования товара превышает 10 символов')
+            self.__name = name
+        except ValueError as exception:
+            print(f'Exception: {exception}')
+
 
     def calculate_total_price(self) -> float:
         """
@@ -27,8 +53,36 @@ class Item:
         c_t_p = self.price * self.quantity
         return c_t_p
 
+
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+
+    @classmethod
+
+
+    def instantiate_from_csv(cls):
+        """
+        Класс метод инициализирующий экземпляры класса Item данными
+        из файла csv_file.      # путь: src items.csv
+        """
+        with open(cls.csv_file, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'],
+                    cls.string_to_number(row['price']),
+                    cls.string_to_number(row['quantity']))
+
+
+    @staticmethod
+
+
+    def string_to_number(num: str):
+        """
+        статический метод, возвращающий число из числа строки
+        """
+        return int(float(num))
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 
+# for 1 homework
 item1 = Item("Смартфон", 10000, 20)
 item2 = Item("Ноутбук", 20000, 3)
-# Item.pay_rate = 0.5
+# for 2 hw
+item = Item('Телефон', 10000, 5)
 
 
 def test_calculate_total_price():
@@ -17,3 +19,21 @@ def test_apply_discount():
     item2.apply_discount()
     assert item1.price == 5000
     assert item2.price == 10000
+
+
+def test_name():
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 8
+    item3 = Item.all[0]
+    assert item3.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
